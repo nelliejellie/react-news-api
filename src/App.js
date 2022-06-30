97d9ae1d5ebb4d5ca83d875e575774129e2b2931
Ewelike Emeka Nelson <emekaewelike@gmail.com>
2022-06-30 16:03:35 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -5,30 +5,36 @@ import Card from './components/Card';
 import Heading from './components/Heading';
 import Pagination from './components/Pagination';
 import axios from 'axios';
+import loadingImage from './images/Loading_2.gif'
 
 function App() {
   const [articles, setArticles] = useState([])
   const [page, setPage] = useState(1)
+  const [loading, setLoaded] = useState(false)
 
   useEffect(()=>{
     fetchArticles()
   },[page])
 
   const fetchArticles = () =>{
+    setLoaded(true)
     axios.get(`https://newsapi.org/v2/everything?q=nigeria&apiKey=f8fd61d8395e419da31226ff9dc65845&pageSize=6&page=${page}`)
       .then(res => {
         setArticles(res.data.articles)
+        setLoaded(false)
       })
       .catch((err)=>{
         console.log(err)
       })
   }
+  
   return (
     <section>
       <Navbar/>
       <Heading/>
-      <Card articles={articles}/>
-      <div className='flex justify-end mr-8 mt-16 mb-4'>
+      {loading === false && <Card articles={articles}/>}
+      {loading && <img src={loadingImage} alt='image' className='w-16 h-16 mx-auto'/>}
+      <div className='flex justify-end mr-8 mt-16 mb-4' >
         <Pagination setPage={setPage} page={page}/>
       </div>
     </section>
